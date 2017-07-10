a8b90bc429fc135def36bc87338f6bee21044b59
Kenny Coyle <kenny.coyle@stv.tv>
2017-07-10 19:12:34 +0100

    initial working items

--- Gemfile
+++ Gemfile
@@ -2,4 +2,6 @@ source 'https://rubygems.org'
 
 gem 'prawn'
 gem 'barby'
-
+gem 'combine_pdf'
+gem 'json'
+gem 'file_utils'
--- badge_from_cli.rb
+++ badge_from_cli.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/Users/kcoyle/.rvm/rubies/ruby-2.4.0/bin/ruby
 
 require 'prawn'
 require 'prawn/measurement_extensions'
@@ -6,15 +6,15 @@ require 'barby'
 require 'barby/barcode/code_128'
 require 'barby/barcode/ean_8'
 require 'barby/outputter/prawn_outputter'
-require 'csv'
+require 'combine_pdf'
+require 'FileUtils'
 
-CSV.foreach("registrations.csv") do |row|
-  userid = row[0].to_i
-  ircnick = row[1]
-  name = row[2] + " " + row[3]
+# Create PDF File
+def create_pdf userid, name, ircnick
+
   aBarcode = Barby::EAN8.new((userid + 1000000).to_s.gsub("100", "000"))
 
-  Prawn::Document.generate(name + ".pdf", :page_size => [180.mm, 180.mm], :margin => 0) do |pdf|
+  Prawn::Document.generate("output/#{name}_1.pdf", :page_size => [180.mm, 180.mm], :margin => 0, :background => "./assets/background1.png") do |pdf|
     pdf.font("/usr/share/fonts/truetype/freefont/FreeSansBold.ttf")
     pdf.text_box name, :at => [10.mm,165.mm], :height => 18.mm, :width => 70.mm, :size => 24, :align => :center, :overflow => :shrink_to_fit, :min_font_size => 14
     pdf.text_box name, :at => [100.mm,165.mm], :height => 18.mm, :width => 70.mm, :size => 24, :align => :center, :overflow => :shrink_to_fit, :min_font_size => 14
@@ -29,4 +29,17 @@ CSV.foreach("registrations.csv") do |row|
     aBarcode.annotate_pdf(pdf, :x => 34.mm, :y => 136.mm, :height => 4.mm)
     aBarcode.annotate_pdf(pdf, :x => 124.mm, :y => 136.mm, :height => 4.mm)
   end
+
+  Prawn::Document.generate("output/#{name}_2.pdf", :page_size => [180.mm, 180.mm], :margin => 0, :background => "./assets/background2.png")
+
+  pdf = CombinePDF.new
+  pdf << CombinePDF.load("output/#{name}_1.pdf") # one way to combine, very fast.
+  pdf << CombinePDF.load("output/#{name}_2.pdf")
+  pdf.save "output/#{name}.pdf"
+
+  FileUtils.rm("output/#{name}_1.pdf")
+  FileUtils.rm("output/#{name}_2.pdf")
+
 end
+
+create_pdf ARGV[0].to_i, ARGV[2], ARGV[1]
